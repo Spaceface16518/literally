5c1acfa4db834e13dfc2c6c0cd45d88b2281d940
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-05-08 23:07:06 -0500

    Add some documentation

--- literally.go
+++ literally.go
@@ -1,17 +1,21 @@
 package literally
 
+//StringPtr takes a string literal and provides a pointer to it
 func StringPtr(literal string) *string {
 	return &literal
 }
 
+//BoolPtr takes a bool literal and provides a pointer to it
 func BoolPtr(literal bool) *bool {
 	return &literal
 }
 
+//IntPtr takes a int literal and provides a pointer to it
 func IntPtr(literal int) *int {
 	return &literal
 }
 
+//UintPtr takes a uint literal and provides a pointer to it
 func UintPtr(literal uint) *uint {
 	return &literal
 }
