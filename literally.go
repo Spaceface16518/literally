5c6de818aab7d1568848bf0daa1c149ab9dd2d4e
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-05-09 09:24:54 -0500

    Fix documentation

--- literally.go
+++ literally.go
@@ -15,22 +15,22 @@ func IntPtr(literal int) *int {
 	return &literal
 }
 
-//Int8Ptr takes a int literal and provides a pointer to it
+//Int8Ptr takes a int8 literal and provides a pointer to it
 func Int8Ptr(literal int8) *int8 {
 	return &literal
 }
 
-//Int16Ptr takes a int literal and provides a pointer to it
+//Int16Ptr takes a int16 literal and provides a pointer to it
 func Int16Ptr(literal int16) *int16 {
 	return &literal
 }
 
-//Int32Ptr takes a int literal and provides a pointer to it
+//Int32Ptr takes a int32 literal and provides a pointer to it
 func Int32Ptr(literal int32) *int32 {
 	return &literal
 }
 
-//Int64Ptr takes a int literal and provides a pointer to it
+//Int64Ptr takes a int64 literal and provides a pointer to it
 func Int64Ptr(literal int64) *int64 {
 	return &literal
 }
@@ -40,22 +40,22 @@ func UintPtr(literal uint) *uint {
 	return &literal
 }
 
-//Uint8Ptr takes a uint literal and provides a pointer to it
+//Uint8Ptr takes a uint8 literal and provides a pointer to it
 func Uint8Ptr(literal uint8) *uint8 {
 	return &literal
 }
 
-//Uint16Ptr takes a uint literal and provides a pointer to it
+//Uint16Ptr takes a uint16 literal and provides a pointer to it
 func Uint16Ptr(literal uint16) *uint16 {
 	return &literal
 }
 
-//Uint32Ptr takes a uint literal and provides a pointer to it
+//Uint32Ptr takes a uint32 literal and provides a pointer to it
 func Uint32Ptr(literal uint32) *uint32 {
 	return &literal
 }
 
-//Uint64Ptr takes a uint literal and provides a pointer to it
+//Uint64Ptr takes a uint64 literal and provides a pointer to it
 func Uint64Ptr(literal uint64) *uint64 {
 	return &literal
 }
